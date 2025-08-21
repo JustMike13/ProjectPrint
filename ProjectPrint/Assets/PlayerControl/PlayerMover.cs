287a65648fb4c12dc8c2c44f700809e362c2726c
JustMike <mihaitza165@gmail.com>
2025-08-21 22:27:10 +0300

    Gravity affects player

--- ProjectPrint/Assets/PlayerControl/PlayerMover.cs
+++ ProjectPrint/Assets/PlayerControl/PlayerMover.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class PlayerMover : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 2f;
     CharacterController characterController;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +19,13 @@ public class PlayerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = characterController.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Small downward force to keep grounded
+        }
+
         float zAxys = 0;
         float xAxys = 0;
 
@@ -22,5 +35,9 @@ public class PlayerMover : MonoBehaviour
         xAxys += Input.GetKey(KeyCode.A) ? -1 : 0;
 
         characterController.Move((xAxys * transform.right + zAxys * transform.forward) * Time.deltaTime * movementSpeed);
+
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
