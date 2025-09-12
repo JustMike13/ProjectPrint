0b0e0eb0d9094263a3721cd06b60f36038306688
JustMike <mihaitza165@gmail.com>
2025-09-12 23:43:24 +0300

    Add shop functionality

---
+++ ProjectPrint/Assets/Shops/Shop.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class ProductPricePair
+{
+    public GameObject product;
+    public float price;
+}
+public class Shop : MonoBehaviour
+{
+    [SerializeField] List<ProductPricePair> Inventory = new List<ProductPricePair> ();
+    [SerializeField] GameObject buttonPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float h = Inventory.Count/2;
+        for (int i = 0; i < Inventory.Count; i++)
+        {
+            GameObject buttonGO = Instantiate(buttonPrefab, transform);
+            buttonGO.transform.position = buttonGO.transform.parent.position + new Vector3(0, (h - i) * 100, 0);
+            TMP_Text buttonText = buttonGO.GetComponentInChildren<TMP_Text>();
+            if (buttonText != null)
+            {
+                buttonText.text = "Buy " + Inventory[i].product.GetComponent<InteractableObject>().Name;
+            }
+
+            // Add listener to the button
+            Button buttonComponent = buttonGO.GetComponent<Button>();
+            if (buttonComponent != null)
+            {
+                int index = i; 
+                buttonComponent.onClick.AddListener(() => Buy(index));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Buy(int pos)
+    {
+        GameObject GO = Inventory[pos].product;
+        if (CurrencySystem.CanAfford(Inventory[pos].price))
+        {
+            CurrencySystem.Spend(Inventory[pos].price);
+            Instantiate(GO, ShopSpawner.Instance.transform);
+        }
+    }
+}
