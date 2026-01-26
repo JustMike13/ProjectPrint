39ad210b0eb3b96f669d9631dbae429d8b4ca5af
JustMike <mihaitza165@gmail.com>
2026-01-27 00:18:51 +0200

    Limit up-down movement of the camera

--- ProjectPrint/Assets/PlayerControl/CameraMover.cs
+++ ProjectPrint/Assets/PlayerControl/CameraMover.cs
@@ -1,20 +1,29 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Progress;
 
 public class CameraMover : MonoBehaviour
 {
-    [SerializeField] InputActionAsset inputActions;
+    [Tooltip("Left-right sensitivity of the camera")]
     [SerializeField] float xSensitivity = 2f;
+    [Tooltip("Up-down sensitivity of the camera")]
     [SerializeField] float ySensitivity = 2f;
+    [Tooltip("Limits up-down movement of the camera")]
+    [SerializeField] Vector2 CameraBoundsYLimits = new Vector2(45f, 45f);
 
     InputAction Look;
+    float pitch;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         Look = InputSystem.actions.FindAction("Look");
+
+        // Initialize pitch from local euler, convert to -180..180 range
+        pitch = transform.localEulerAngles.x;
+        if (pitch > 180f) pitch -= 360f;
+        CameraBoundsYLimits = new Vector2(-CameraBoundsYLimits.x, -CameraBoundsYLimits.y);
     }
 
     // Update is called once per frame
@@ -24,7 +33,15 @@ public class CameraMover : MonoBehaviour
 
         Vector2 look = Look.ReadValue<Vector2>();
 
-        transform.parent.Rotate(new Vector3(0, look[0] * xSensitivity, 0));
-        transform.Rotate(new Vector3(-look[1] * ySensitivity, 0, 0));
+        // Yaw - rotate parent around Y
+        transform.parent.Rotate(new Vector3(0f, look.x * xSensitivity, 0f));
+
+        // Pitch - update tracked pitch and clamp to configured bounds
+        float deltaPitch = -look.y * ySensitivity;
+
+        pitch = Mathf.Clamp(pitch + deltaPitch, CameraBoundsYLimits.y, CameraBoundsYLimits.x);
+
+        // Apply clamped pitch to local rotation (preserve Y/Z as zero for the camera local rotation)
+        transform.localEulerAngles = new Vector3(pitch, 0f, 0f);
     }
 }
